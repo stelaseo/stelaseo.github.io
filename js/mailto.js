c5eb9875339aff3d2cbae459b0ad416f8f0a6d76
Stela H. Seo <stela.h.seo@gmail.com>
2021-03-26 12:00:15 -0500

    Import from the old server

---
+++ js/mailto.js
@@ -0,0 +1,23 @@
+$(document).ready( function( )
+{
+    var json = $.getJSON( "contents/MailTo.json", function( )
+    {
+        var myName = $("#myName");
+        json = json["responseJSON"];
+        if( myName != undefined && myName != null && "email" in json )
+        {
+            var addr = json["email"].replace( "@A@T@", "@" );
+            for( var i = 0; i < 3; i++ )
+            {
+                addr = addr.replace( "#D#O#T#", "." );
+            }
+            myName.attr( "href", "mailto:" + addr );
+            myName.attr( "title", "Send an email" );
+            var btnSendEmail = $("#btnSendEmail");
+            if( btnSendEmail != undefined && btnSendEmail != null )
+            {
+                btnSendEmail.attr( "href", "mailto:" + addr );
+            }
+        }
+    } );
+} );
